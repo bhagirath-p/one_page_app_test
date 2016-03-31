ca0f33f3ba5c7320cd08c3c28586766adfb0bb0a
Bhagirath <bpurushothama@qwinix.io>
2016-03-31 10:38:46 +0530

    initial commit

---
+++ scripts/page_builder.js
@@ -0,0 +1,53 @@
+jQuery.fn.extend({
+	buildBAse: function() {
+		$(".base").append($("<div/>", {
+			"class" : "base-layout"
+		})
+			.append($("<div/>", {
+				"class" : "layout"
+			})
+				.append($("<div/>", {
+					"class" : "header",
+					"id"    : "header"
+				}))
+				.append($("<div/>", {
+					"class" : "nav",
+					"id"    : "nav"
+				}))
+				.append($("<div/>", {
+					"class" : "section",
+					"id"    : "section"
+				}))
+				.append($("<div/>", {
+					"class" : "footer",
+					"id"    : "footer"
+				}))
+			)
+		);
+	},
+    bindClick: function(elementId, functionToExecute) {
+    	$("#"+elementId).click(function() {
+    		functionToExecute;
+    	});
+    },
+    bindHover: function(elementId, functionToExecute) {
+    	$("#"+elementId).hover(function() {
+    		functionToExecute;
+    	});
+    },
+    bindKeyPress: function(elementId, functionToExecute) {
+    	$("#"+elementId).keypress(function() {
+    		functionToExecute;
+    	});
+    },
+    bindKeyUp: function(elementId, functionToExecute) {
+    	$("#"+elementId).keyup(function() {
+    		functionToExecute;
+    	});
+    },
+    bindKeyDown: function(elementId, functionToExecute) {
+    	$("#"+elementId).keydown(function() {
+    		functionToExecute;
+    	});
+    },
+});
